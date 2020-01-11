76d5dd5546824ef12b69f89d388ef4c695717005
Jayson Kesterson <jayson.kesterson24@gmail.com>
2020-01-11 16:32:50 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_day_genre_season_to_show.rb
+++ db/migrate/007_add_day_genre_season_to_show.rb
@@ -1,5 +1,5 @@
-class AddCatchphraseToCharacters < ActiveRecord::Migration[5.1]
+class AddAttributesToCharacters < ActiveRecord::Migration[5.1]
   def change
-    add_column :characters, :catchphrase, :string
+    add_column :shows, :day, :string
   end
 end

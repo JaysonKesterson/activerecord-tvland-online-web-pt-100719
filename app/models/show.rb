f062a5fbccda3159bb3752fbcb87fdde4d536218
Jayson Kesterson <jayson.kesterson24@gmail.com>
2020-01-11 16:23:53 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
   has_many :characters
+  has_many :actors, through: :characters
+  
+  def actors_list
+    self.characters.actors.collect do |actor|
+      actor.name
+    end
+  end
 end
